35d026cfd1e36a45023a19bd39b94216d502e507
Lekha karthik <lekhakarthik1392@gmail.com>
2023-04-05 12:56:26 +0530

    added ;

--- shuffledeck.py
+++ shuffledeck.py
@@ -1,7 +1,7 @@
 import itertools, random
 deck=list(itertools.product(range(1,14),['spade','Heart','Diamond','club',]))
 random.shuffle(deck)
-print("player 1 got:")
+print("player 1 got:");
 for i in range(13):
     print(deck[i][0],"of",deck[i][1])
 random.shuffle(deck)    
