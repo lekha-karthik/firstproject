9329c416a62c870d0f801c14295eed983f8aa710
Lekha karthik <lekhakarthik1392@gmail.com>
2023-04-05 11:59:30 +0530

    changed to 1 player to 2 players

--- shuffledeck.py
+++ shuffledeck.py
@@ -1,6 +1,10 @@
 import itertools, random
-deck=list(itertools.product(range(1,14),['spade','Heart','Diamond','club','round']))
+deck=list(itertools.product(range(1,14),['spade','Heart','Diamond','club',]))
 random.shuffle(deck)
-print("you got:")
-for i in range(8):
+print("player 1 got:")
+for i in range(13):
+    print(deck[i][0],"of",deck[i][1])
+random.shuffle(deck)    
+print("player 2 got:")
+for i in range(13):
     print(deck[i][0],"of",deck[i][1])
